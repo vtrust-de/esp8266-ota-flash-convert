a6a110f9b7debd5dafad6de9b3c4d0f43647e5ff
Colin Kuebler <kueblc@gmail.com>
2019-03-21 14:38:12 -0400

    Added build and clean scripts that I use to build on linux Still not ideal but more portable than before

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+ARDUINO_PATH="/esp/arduino"
+#FLASH_PROPERTY="custom_eesz"
+FLASH_PROPERTY="custom_FlashSize"
+
+mkdir -p build
+
+build_user () {
+	echo "==============="
+	echo "Compiling user$1"
+	echo "==============="
+	$ARDUINO_PATH --pref build.path=build/user$1 --pref $FLASH_PROPERTY=generic_1M0_$1 --verify esp8266-ota-flash-convert/esp8266-ota-flash-convert.ino &&
+	esptool.py elf2image --version 2 build/user$1/esp8266-ota-flash-convert.ino.elf &&
+	mv build/user$1/esp8266-ota-flash-convert.ino-0x*1000.bin .
+}
+
+build_user 1
+build_user 2
+
+echo "================="
+echo "Packaging Upgrade"
+echo "================="
+./create_upg.py
+
---
+++ clean.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+rm -r build esp8266-ota-flash-convert*.bin
